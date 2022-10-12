88bbae8d5a1235b9be73ad6c881272b8895c9109
bninamango.personal <bninamango.personal@gmail.com>
2022-10-12 16:01:51 -0500

    ...

--- Player/Player/Player.cs
+++ Player/Player/Player.cs
@@ -4,23 +4,23 @@ using System.Text;
 
 namespace bninamango
 {
-    public class Player
+    class Player
     {
         private char character;
         private Vector2 position;
 
-        public Player(char character, Vector2 position) 
+        public Player(char character, Vector2 position)
         {
             this.character = character;
             this.position = position;
         }
 
-        public void Move(Vector2 direction) 
+        public void Move(Vector2 direction)
         {
             position += direction;
         }
 
-        public void Draw() 
+        public void Draw()
         {
             position.x = (position.x >= 0) ? position.x : 0;
             position.y = (position.y >= 0) ? position.y : 0;
--- Player/Player/Vector2.cs
+++ Player/Player/Vector2.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace bninamango
 {
-    public class Vector2
+    class Vector2
     {
         public int x;
         public int y;
@@ -34,10 +34,22 @@ namespace bninamango
             get => new Vector2(-1, 0);
         }
 
+        public Vector2() 
+        {
+            x = 0;
+            y = 0;
+        }
+
         public Vector2(int x, int y)
         {
             this.x = x;
             this.y = y;
+        }
+
+        public Vector2(Vector2 vector)
+        {
+            x = vector.x;
+            y = vector.y;
         }
 
         public static Vector2 operator +(Vector2 a, Vector2 b)
